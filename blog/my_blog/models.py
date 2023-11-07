3b5afa851ffd6ac86e755fbe763243081678ea6f
desirelesson <theproblemdi@gmail.com>
2023-11-07 16:37:03 +0300

    add manager in models.py

--- blog/my_blog/models.py
+++ blog/my_blog/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
+    
+
+   
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -13,7 +23,11 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True) 
-    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT) 
+
+
+    objects = models.Manager() # менеджер, применяемый по умолчанию
+    published = PublishedManager() 
     
 
     class Meta:
